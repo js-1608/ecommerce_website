04218cc642e99a5732b44e36dec39e156e5d0c5a
juhi2116 <141029609+js-1608@users.noreply.github.com>
2026-01-07 22:26:48 +0530

    updated

---
+++ frontend/src/components/Products/ProductDetail.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ProductDetail = () => {
+  return (
+    <div className="max-w-6xl mx-auto bg-white p-8 rounded-lg ">
+      <div className="flex felx-col md:flex-row">
+        {/* left thumbanil */}
+        <div className="hidden md:flex flex-col space-y-4 mr-6"></div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetail;
