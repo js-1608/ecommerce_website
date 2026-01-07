711feb8c57a9db5e6f1e14e3ad3d092065e15df2
juhi2116 <141029609+js-1608@users.noreply.github.com>
2026-01-07 22:26:48 +0530

    updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import react from "react";
 import Hero from "../components/Layout/Hero";
 import GenderCollection from "../components/Products/GenderCollection";
 import NewArrival from "../components/Products/NewArrival";
+import ProductDetail from "../components/Products/ProductDetail";
 
 export default function Home() {
     return (
@@ -9,6 +10,9 @@ export default function Home() {
             <Hero />
             <GenderCollection/>
             <NewArrival/>
+            {/* best seller */}
+            <h2 className="text-center text-4xl font-bold mb-4">Best Seller</h2>
+            <ProductDetail/>
         </>
     );
 }
