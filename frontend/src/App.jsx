bf2445556c41ce1361ce9f149745122b964e9512
juhi2116 <141029609+js-1608@users.noreply.github.com>
2026-01-14 00:05:14 +0530

    updated product detail and u may also like section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 // pages
 import Home from './pages/Home'
 import UserLayout from './components/Layout/UserLayout'
-
+import {Toaster} from "sonner"
 function App() {
 
 
   return (
     <BrowserRouter>
+    <Toaster position="top-right"/>
       <Routes>
         {/* user Route */}
         <Route path='/' element={<UserLayout />}>
