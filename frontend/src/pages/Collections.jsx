7d92f0c321d7014aec03b252e46594f0755a5ca6
“shivam” <2200520140049@ietlucknow.ac.in>
2026-02-04 01:09:34 +0530

    working on sidebar

--- frontend/src/pages/Collections.jsx
+++ frontend/src/pages/Collections.jsx
@@ -1,10 +1,87 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useEffect, useState } from 'react'
+import { FaFilter } from "react-icons/fa"
 
-const Collections= () => {
+
+const Collections = () => {
+
+
+  const [product, setProoduct] = useState([]);
+  const sideBarRef = useRef(null);
+  const [isSideBarOpen, setIsSideBarOpen] = useState(true)
+  const toggleSideBar = () => {
+    setIsSideBarOpen(!isSideBarOpen);
+  }
+
+const handleClickOutside = (e) => {
+  if (sideBarRef.current && !sideBarRef.current.contains(e.target)) {
+    setIsSideBarOpen(false);
+  }
+};
+
+
+useEffect(() => {
+  document.addEventListener("mousedown", handleClickOutside);
+
+  return () => {
+    document.removeEventListener("mousedown", handleClickOutside);
+  };
+}, []);
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      const ProductDetailArray = [
+        {
+          _id: 1,
+          name: "product 1",
+          price: 100,
+          images: [{ url: "https://picsum.photos/seed/picsum/200/300" }],
+        },
+        {
+          _id: 2,
+          name: "product 2",
+          price: 120,
+          images: [{ url: "https://picsum.photos/id/25/200/300" }],
+        },
+        {
+          _id: 3,
+          name: "product 3",
+          price: 150,
+          images: [{ url: "https://picsum.photos/id/20/200/300" }],
+        },
+        {
+          _id: 4,
+          name: "product 4",
+          price: 200,
+          images: [{ url: "https://picsum.photos/id/3/200/300" }],
+        },
+      ];
+      setProoduct(ProductDetailArray)
+    }, 1000)
+  }, [])
+
   return (
-    <div>
-      col
-    </div>
+    <>
+      <div className='flex flex-col items-end'>
+        {/* mobile filter button */}
+       <button
+          onClick={toggleSideBar}
+          className='lg:hidden w-20 border p-2 flex justify-center items-center'
+        >
+          <FaFilter className='mr-2' />
+        </button>
+
+
+
+      </div>
+
+      {/* filter side bar */}
+      <div ref={sideBarRef} className={`max-w-1/3 h-full  bg-white ${isSideBarOpen ? "translate-x-0" : "-translate-x-full"} fixed inset-y-0 z-50 left-0 w-64 bg-white overflow-y-auto transition-transform duration-300`} >
+        {/* <FilterSidebar/> */}
+        sidebar
+      </div>
+    </>
   )
 }
 
